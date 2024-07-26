90d198c599d5e2f9e3d615f59cc59fc398db7abd
tanchajian <40681816+tanchajian@users.noreply.github.com>
2024-07-26 18:51:02 +0800

    Update server.py

--- server.py
+++ server.py
@@ -10,13 +10,13 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        msg = 'Hello At-Eases!! you requested %s' % (self.path)
+        msg = 'Hello At-Eases!!! you requested %s' % (self.path)
         self.wfile.write(msg.encode())
 
     def do_POST(self):
         length = int(self.headers['Content-Length'])
         post_data = urllib.parse.parse_qs(self.rfile.read(length).decode('utf-8'))
-        self.wfile.write(post_data)
+        print(post_data)
 
 
 port = int(os.getenv('PORT', 80))
